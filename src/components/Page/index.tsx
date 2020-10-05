f070af2afbd618950e9853ba36a7ab279cfa6c97
Maximiano Eduardo <edu.maxi.96@gmail.com>
2020-10-04 22:44:23 -0300

    🚧 Default Models

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
 import DefaultOverlayContent from '../DefaultOverlayContent';
+import { ModelSection, ModelsWrapper } from '../Model';
 
 import { Container } from './styles';
 
 const Page: React.FC = () => {
   return (
     <Container>
-      <ModalsWrapper>
+      <ModelsWrapper>
         <div>
-          <ModalSection 
-            modelName='Model One'
-            overlayNode={
-              <DefaultOverlayContent
-                label="Model One"
-                description="Order Online for Delivery"
-              />
-            }
-          />
+          {[
+            'Model One',
+            // 'Model Two',
+            // 'Model Three',
+            // 'Model Four',
+            // 'Model Five',
+            // 'Model Six',
+            // 'Model Seven',
+          ].map(modelName => (
+
+            <ModelSection
+              className='colored' 
+              modelName={modelName}
+              key={modelName}
+              overlayNode={
+                <DefaultOverlayContent
+                  label={modelName}
+                  description="Order Online for Delivery"
+                />
+              }
+            />
+
+          ))}
+          
 
         </div>
-      </ModalsWrapper>
+      </ModelsWrapper>
     </Container>
   );
 };
