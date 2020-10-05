19360ee39ef21d85765c088484a1edcf30eee92f
Maximiano Eduardo <edu.maxi.96@gmail.com>
2020-10-04 22:44:07 -0300

    🔥 Context API React

---
+++ src/components/Model/ModelOverlay/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    position: sticky;
+    height: 100vh;
+    margin-top: -100vh;
+    top: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center
+`;
