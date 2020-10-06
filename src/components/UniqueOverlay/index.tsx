b96d261496072d67024a2006164b49743463cd6b
Maximiano Eduardo <edu.maxi.96@gmail.com>
2020-10-05 22:06:36 -0300

    🔥Unique Overlay
    header and Svg

--- src/components/UniqueOverlay/index.tsx
+++ src/components/UniqueOverlay/index.tsx
@@ -1,11 +1,36 @@
+import { useTransform } from 'framer-motion'
 import React from 'react'
+import useWrapperScroll from '../Model/useWrapperScroll'
 
-import { Container } from './styles'
+import { Container, Header, Logo, Burger, Footer } from './styles'
 
 const UniqueOverlay: React.FC = () => {
+
+  const { scrollYProgress } = useWrapperScroll()
+
+  const opacity = useTransform(scrollYProgress, [0.9, 1], [0,1])
+
   return (
     <Container>
-      <h1>UniqueOverlay</h1>
+      <Header>
+        <Logo />
+        <Burger />
+      </Header>
+
+      <Footer style={{opacity}}>
+        <ul>
+          <li>
+            <a href="#">UI Clone</a>
+          </li>
+          <li>
+            <a href="#">UI Clone</a>
+          </li>
+          <li>
+            <a href="#">UI Clone</a>
+          </li>
+        </ul>
+      </Footer>
+
     </Container>
   )
 }
